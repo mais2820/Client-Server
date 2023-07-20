ffd55df921b14a3487aa591f30e4404acf9a350e
Al Mais <mais2820@gmail.com>
2023-07-20 15:56:09 +0700

    CRUD Role

--- API/Program.cs
+++ API/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddScoped<IAccountRoleRepository, AccountRoleRepository>();
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 builder.Services.AddScoped<IEducationRepository, EducationRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
