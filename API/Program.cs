c8e572211bd0473e790b082a56da60bfa5ac92ec
Al Mais <mais2820@gmail.com>
2023-07-20 14:58:44 +0700

    CRUD Account

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<BookingDbContext>(option => option.UseSqlServer(co
 
 // Add repositories to the container.
 builder.Services.AddScoped<IUniversityRepository, UniversityRepository>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
